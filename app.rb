e1b939c87bb94b1c1caeee6532b9274021d3c4b5
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-09-20 22:51:42 +0000

    les liens met en app.rb

--- app.rb
+++ app.rb
@@ -5,3 +5,23 @@ require "http"
 get("/") do
   erb(:homepage)
 end
+
+get("/square/new") do
+  erb(:homepage)
+end
+
+get("/square_root/new") do
+  erb(:homepage)
+end
+
+get("/random/new") do
+  erb(:homepage)
+end
+
+get("/payment/new") do
+  erb(:homepage)
+end
+
+get("/square/results") do
+  erb(:square_results)
+end
