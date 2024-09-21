ad83ed824dd8c96d3512aa9289d2124bd0cfcae3
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-09-21 01:46:05 +0000

    corrige l'erreur

--- app.rb
+++ app.rb
@@ -34,20 +34,25 @@ get("/square_root/results") do
 erb(:squareroot_results)
 end
 
-get("/payment/results") do
+get("/payment/results") do 
   @raw_apr = params.fetch("apr").to_i
   @raw_years = params.fetch("years").to_i
   @raw_principal = params.fetch("principal").to_i
 
-  @apr = format('%.4f', @raw_apr.to_i)
+  @apr = format('%.4f', @raw_apr.to_f)  
   @years = @raw_years
-  @principal = format('%.2f', params.fetch("principal").to_i)
+  @principal = format('%.2f', @raw_principal.to_f)
 
-  @rate = @raw_apr / 100
+  @rate = @raw_apr / 100.0  
   @n = (@years * 12)
   p1 = (@rate * @raw_principal)
-  p2 = (1-(1 + @rate))
-  @payment = p1 / p2
+  p2 = (1 - (1 + @rate)**(-@n))  
+
+  if p2.zero?
+    @payment = 0 
+  else
+    @payment = p1 / p2
+  end
 
   erb(:payment_results)
 end
